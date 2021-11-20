184d9b440edc054d8335c0342f349e29c245d874
QSP <QSP@QSP>
2021-11-20 16:45:18 +0530

    Hi

---
+++ src/test/java/testscripts/Test5.java
@@ -0,0 +1,12 @@
+package testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+	@Test
+	public void tc2() {
+		Reporter.log("Hello APItesting",true);
+	}
+
+}
