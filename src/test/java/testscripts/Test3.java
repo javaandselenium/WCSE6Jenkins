d3145e70f914652fb89c5a215bfca4db35c6fecf
QSP <QSP@QSP>
2021-11-20 16:29:27 +0530

    Hello

---
+++ src/test/java/testscripts/Test3.java
@@ -0,0 +1,12 @@
+package testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void tc2() {
+		Reporter.log("Hello Python",true);
+	}
+
+}
---
+++ src/test/java/testscripts/Test4.java
@@ -0,0 +1,12 @@
+package testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 {
+	@Test
+	public void tc2() {
+		Reporter.log("Hello Appium",true);
+	}
+
+}
